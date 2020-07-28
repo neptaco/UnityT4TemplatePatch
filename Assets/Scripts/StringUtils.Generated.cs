90134c0c9e7ef1d50b09f3d88ae8ecf286bd31d0
neptaco <neptacox@gmail.com>
2020-07-28 23:23:18 +0900

    Initial Commit

---
+++ Assets/Scripts/StringUtils.Generated.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+
+public class StringUtils 
+{
+    public static string Concat<T1>(T1 v1)
+    {
+        var sb = new StringBuilder();
+        sb.Append(v1);
+        return sb.ToString();
+    }
+
+    public static string Concat<T1, T2>(T1 v1, T2 v2)
+    {
+        var sb = new StringBuilder();
+        sb.Append(v1).Append(v2);
+        return sb.ToString();
+    }
+
+    public static string Concat<T1, T2, T3>(T1 v1, T2 v2, T3 v3)
+    {
+        var sb = new StringBuilder();
+        sb.Append(v1).Append(v2).Append(v3);
+        return sb.ToString();
+    }
+
+    public static string Concat<T1, T2, T3, T4>(T1 v1, T2 v2, T3 v3, T4 v4)
+    {
+        var sb = new StringBuilder();
+        sb.Append(v1).Append(v2).Append(v3).Append(v4);
+        return sb.ToString();
+    }
+
+    public static string Concat<T1, T2, T3, T4, T5>(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5)
+    {
+        var sb = new StringBuilder();
+        sb.Append(v1).Append(v2).Append(v3).Append(v4).Append(v5);
+        return sb.ToString();
+    }
+
+    public static string Concat<T1, T2, T3, T4, T5, T6>(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6)
+    {
+        var sb = new StringBuilder();
+        sb.Append(v1).Append(v2).Append(v3).Append(v4).Append(v5).Append(v6);
+        return sb.ToString();
+    }
+
+    public static string Concat<T1, T2, T3, T4, T5, T6, T7>(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7)
+    {
+        var sb = new StringBuilder();
+        sb.Append(v1).Append(v2).Append(v3).Append(v4).Append(v5).Append(v6).Append(v7);
+        return sb.ToString();
+    }
+
+    public static string Concat<T1, T2, T3, T4, T5, T6, T7, T8>(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8)
+    {
+        var sb = new StringBuilder();
+        sb.Append(v1).Append(v2).Append(v3).Append(v4).Append(v5).Append(v6).Append(v7).Append(v8);
+        return sb.ToString();
+    }
+
+    
+}
---
+++ README.md
@@ -0,0 +1,36 @@
+# Unity T4 Template Patch
+
+Using T4 Template by Unity generated csproj.
+
+## Install
+
+### Package Manager
+
+```manifest.json
+{
+    "dependencies": {
+        "com.xtaltools.unity-t4-patch": "https://github.com/neptaco/UnityT4TemplatePatch.git?path=Packages/T4TemplatePatch"
+    }
+}
+```
+
+## Patching to Csproj
+
+Search for files with extension `.tt` and generate an ItemGroup tag.
+
+Generated from `Assets/Scripts/Scripts/StringUtils.Generated.tt` as follows
+
+
+```Assembly-Csharp.csproj:xml
+  <ItemGroup>
+    <Content Include="Assets\Scripts\StringUtils.Generated.tt">
+      <Generator>TextTemplatingFileGenerator</Generator>
+      <LastGenOutput>StringUtils.Generated.cs</LastGenOutput>
+    </Content>
+  </ItemGroup>
+```  
+
+
+### License
+
+This library is under the MIT License.
